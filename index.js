e862dec8bab17fc710885c9d77aececfc3d01e16
ait-oubraim <brahim.ait.oubraim.work@gmail.com>
2025-10-18 23:00:45 +0100

    add all files

---
+++ index.js
@@ -0,0 +1,10 @@
+// const timerText = document.querySelector('.workSection .container .countdown-timer');
+// let pomoTime = 5;
+// let seconds =0;
+
+// // setInterval(() => {
+// //     for(let i=0; i<= pomoTime;){
+// //         console.log(`${pomoTime} : ${seconds}`)
+// //     }
+// // }, 1000);
+
